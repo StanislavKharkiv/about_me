6bf464674fdbf6576d4a55f4da7674f1aab29698
StanislavKharkiv <shtas89@gmail.com>
2025-06-05 18:25:44 +0200

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,54 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from "@eslint/eslintrc"
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "max-len": ["error", { code: 120 }],
+      "semi": ["error", "never"],
+      "quotes": ["error", "double", { avoidEscape: true }],
+      "quote-props": ["error", "consistent-as-needed"],
+      "no-multiple-empty-lines": ["error", { max: 1 }],
+      "eol-last": ["error", "always"],
+      "indent": ["error", 2, { SwitchCase: 1 }],
+      "space-before-blocks": ["error", "always"],
+      "keyword-spacing": ["error", { before: true, after: true }],
+      "no-tabs": "error",
+      "comma-dangle": ["error", "always-multiline"],
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "import/order": [
+        "error",
+        {
+          "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+          "alphabetize": { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "import", next: "*" },
+        { blankLine: "any", prev: "import", next: "import" },
+        { blankLine: "always", prev: "*", next: "function" },
+        { blankLine: "always", prev: "*", next: "export" },
+        { blankLine: "always", prev: "*", next: "class" },
+        { blankLine: "always", prev: "*", next: "const" },
+        { blankLine: "always", prev: "*", next: "let" },
+        { blankLine: "always", prev: "*", next: "var" },
+      ],
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig

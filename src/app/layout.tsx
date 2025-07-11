8ae6a8fb3ab4240d9f237b5e73528309f792de98
StanislavKharkiv <shtas89@gmail.com>
2025-07-12 01:07:48 +0200

    feat:home screen with text animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 
-import "@/styles/reset.scss"
+import "@/styles/global.scss"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/components/HomeScreen/HomeScreen.tsx
+++ src/components/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,10 @@
 "use client"
 
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { useRef } from "react"
+
+import CodeRedactor from "../general/CodeRedactor"
 import CodeTyping from "../general/CodeTyping"
 
 import styles from "./HomeScreen.module.scss"
@@ -26,11 +31,30 @@ const codeLines = [
 ]
 
 export default function HomeScreen() {
+  const header = useRef<HTMLHeadingElement>(null)
+  useGSAP(() => {
+    const tl = gsap.timeline()
+    if (!header.current) return
+    const firstPart = header.current.firstChild
+    const lastPart = header.current.lastChild
+    tl.fromTo(firstPart, { y: "-500%", opacity: -1 }, { y: "0%", opacity: 1, duration: 2, ease: "power3.out" })
+
+    tl.fromTo(
+      lastPart,
+      { y: "500%", opacity: -1 },
+      { y: "0%", opacity: 1, duration: 2, ease: "power3.out" },
+      "<", // This starts the animation at the same time as the previous one
+    )
+  })
   return (
     <div className={styles.container}>
-      <CodeTyping codeLines={codeLines} className={styles.codeStyles} />
+      <CodeRedactor className={styles.codeStyles}>
+        <CodeTyping codeLines={codeLines} />
+      </CodeRedactor>
       <section className={styles.background}>
-        <h1 className={styles.mainHeader}>Web Development</h1>
+        <h1 className={styles.mainHeader} ref={header}>
+          <span>Web</span> <span className={styles.verticalLine} /> <span>Development</span>
+        </h1>
       </section>
     </div>
   )

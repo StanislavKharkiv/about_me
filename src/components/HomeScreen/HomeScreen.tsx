53ef5406620847d35fd06da9b6295df8542e2fd5
StanislavKharkiv <shtas89@gmail.com>
2026-07-29 15:58:21 +0200

    feat: restyle home page: add header, change titles

--- src/components/HomeScreen/HomeScreen.tsx
+++ src/components/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import { useGSAP } from "@gsap/react"
+import clsx from "clsx"
 import gsap from "gsap"
+import { Folder, FolderOpen, Phone, PhoneOutgoing } from "lucide-react"
+import Link from "next/link"
 import { useRef } from "react"
 
 import CodeRedactor from "../general/CodeRedactor"
 import CodeTyping from "../general/CodeTyping"
+import Header from "../general/Header"
 import NeonLights from "../general/NeonLights"
 
 import { codeLines } from "./constants"
@@ -47,28 +51,43 @@ export default function HomeScreen() {
   ]
 
   return (
-    <div className={styles.container}>
-      <NeonLights linesData={lines} />
-      <CodeRedactor className={styles.codeStyles}>
-        <CodeTyping codeLines={codeLines} />
-      </CodeRedactor>
-      <section className={styles.textBlock}>
-        <p className={styles.slogan}>Turning ideas into interactive web products.</p>
-        <h1 className={styles.mainHeader} ref={header}>
-          <span>Web</span> <span className={styles.verticalLine} /> <span>Development</span>
-        </h1>
-        <h2 className={styles.title}>
-          <svg>
-            <text x="50%" y="50%" textAnchor="middle" dominantBaseline="middle">
-              By Stanislav Iosyfov
-            </text>
-          </svg>
-        </h2>
-        <div className={styles.horizontalLine} />
-        <div className={styles.technologies} ref={technologies}>
-          {defaultTextTechnologies}
-        </div>
-      </section>
+    <div className={styles.pageContainer}>
+      <Header />
+      <div className={styles.container}>
+        <NeonLights linesData={lines} />
+        <CodeRedactor className={styles.codeStyles}>
+          <CodeTyping codeLines={codeLines} />
+        </CodeRedactor>
+        <section className={styles.textBlock}>
+          <p className={styles.slogan}>Turning ideas into interactive web products</p>
+          <h1 className={styles.mainHeader} ref={header}>
+            <span>BUILDING</span> <span>DIGITAL EXPERIENCES</span>
+          </h1>
+          <h2 className={styles.title}>
+            <svg>
+              <text x="0" y="50%" textAnchor="start" dominantBaseline="middle">
+                Software Engineer & Full Stack Developer
+              </text>
+            </svg>
+          </h2>
+          <div className={styles.btnWrap}>
+            <Link href={"/portfolio"} className={clsx(styles.btnLink)} aria-label={"Open portfolio"}>
+              <span>VIEW PROJECTS</span>
+              <Folder className={styles.defaultIcon} size={18} strokeWidth={1} />
+              <FolderOpen className={styles.hoveredIcon} size={18} strokeWidth={1} />
+            </Link>
+            <Link href={"/contacts"} className={styles.btnLink} aria-label={"Open contact form"}>
+              <span>CONTACT ME</span>
+              <Phone className={styles.defaultIcon} size={18} strokeWidth={1} />
+              <PhoneOutgoing className={styles.hoveredIcon} size={18} strokeWidth={1} />
+            </Link>
+          </div>
+          <div className={styles.horizontalLine} />
+          <div className={styles.technologies} ref={technologies}>
+            {defaultTextTechnologies}
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
